344571178a5bc4164c46128f593ae472db285ba4
Logan Bynes <logan.bynes@gmail.com>
2018-10-31 15:15:37 -0600

    Inital commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
